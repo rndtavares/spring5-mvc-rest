178a3540afe7247627b97b00626e7512c72fe92f
Ronaldo <rndtavares@gmail.com>
2018-06-19 08:03:59 -0300

    vendor swagger assignment by rnd

--- src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
@@ -1,6 +1,8 @@
 package guru.springfamework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VendorDTO {
+    @ApiModelProperty(value = "The vendor's name", required = true)
     private String name;
 
+    @ApiModelProperty(value = "The vendor's url")
     @JsonProperty("vendor_url")
     private String vendorUrl;
 
--- src/main/java/guru/springfamework/controllers/v1/VendorController.java
+++ src/main/java/guru/springfamework/controllers/v1/VendorController.java
@@ -3,12 +3,15 @@ package guru.springfamework.controllers.v1;
 import guru.springfamework.api.v1.model.VendorDTO;
 import guru.springfamework.api.v1.model.VendorListDTO;
 import guru.springfamework.services.VendorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jt on 10/6/17.
  */
+@Api(description = "This is my vendor API")
 @RestController
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
@@ -21,36 +24,43 @@ public class VendorController {
         this.vendorService = vendorService;
     }
 
+    @ApiOperation(value = "This will get a list of vendors", notes = "Notes about get a list of vendors")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public VendorListDTO getVendorList(){
         return vendorService.getAllVendors();
     }
 
+    @ApiOperation(value = "This will get a unique vendor", notes = "Notes about getting a unique vendor")
     @GetMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO getVendorById(@PathVariable Long id){
         return vendorService.getVendorById(id);
     }
 
+    @ApiOperation(value = "This will create a new vendor", notes = "Notes about creating a new vendor")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public VendorDTO createNewVendor(@RequestBody VendorDTO vendorDTO){
         return vendorService.createNewVendor(vendorDTO);
     }
 
+    @ApiOperation(value = "This will update a verdor", notes = "Notes about updating a vendor")
     @PutMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO updateVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO){
         return vendorService.saveVendorByDTO(id, vendorDTO);
     }
 
+    @ApiOperation(value = "This will update by id the fields in the vendor that is not null",
+            notes = "Notes about patching a vendor")
     @PatchMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO patchVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO){
         return vendorService.saveVendorByDTO(id, vendorDTO);
     }
 
+    @ApiOperation(value = "This will delete a vendor", notes = "Notes about deleting a vendor")
     @DeleteMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public void deleteVendor(@PathVariable Long id){
